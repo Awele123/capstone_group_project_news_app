430760f05849cc5b3ed23eb732dc3bac7d9fa080
awele123 <nwabuzorawele42@gmail.com>
2025-05-17 16:52:08 +0100

    new ui

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'UpNext',
-      initialRoute: RouteName.bottomNav,
+      initialRoute: RouteName.signIn,
       navigatorKey: AppRoute.navigatorKey,
       onGenerateRoute: Routes.generateRoute,
       debugShowCheckedModeBanner: false,
